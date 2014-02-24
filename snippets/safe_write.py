aa502fbf1d473c02c085a7d9cf97f97abad006b4
nitetrain8 <nathan.starkweather@gmail.com>
2014-02-24 14:39:39 -0800

    now unittests for data report, primarily for testing pickleability.

--- snippets/safe_write.py
+++ snippets/safe_write.py
@@ -17,21 +17,41 @@ __author__ = 'Nathan Starkweather'
 from pickle import dump as pickle_dump
 from io import StringIO, BytesIO
 from json import dump as json_dump
+from os import makedirs
+from os.path import split as path_split, exists as path_exists
 
 
-def safe_pickle(obj: object, filename: str, **kwargs):
+def safe_pickle(obj, filepath, **kwargs):
+    """
+    @param obj: Any pickleable object
+    @type obj:  T
+    @param filepath: filepath to save the pickle file
+    @type filepath: str
+    @param kwargs: dict of pickle kwargs
+    @type kwargs: dict
+    @return: None
+    @rtype: None
+    """
     temp = BytesIO()
     pickle_dump(obj, temp, **kwargs)
 
-    with open(filename, 'wb') as f:
+    tail, head = path_split(filepath)
+    if not path_exists(tail):
+        makedirs(tail)
+
+    with open(filepath, 'wb') as f:
         f.write(temp.getvalue())
 
 
-def safe_json(obj: object, filename: str, **kwargs):
+def safe_json(obj: object, filepath: str, **kwargs):
     temp = StringIO()
     kwargs['indent'] = kwargs.get('indent', 4)
     json_dump(obj, temp, **kwargs)
 
-    with open(filename, 'w') as f:
+    tail, head = path_split(filepath)
+    if not path_exists(tail):
+        makedirs(tail)
+
+    with open(filepath, 'w') as f:
         f.write(temp.getvalue())
 
