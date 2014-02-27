1f82cd383088d4e8ff46ed5fffb310a094338608
nitetrain8 <nathan.starkweather@gmail.com>
2014-02-27 14:36:19 -0800

    pre-commit to creating mock strptime

--- snippets/__init__.py
+++ snippets/__init__.py
@@ -1,14 +1,16 @@
-__author__ = 'Nathan Starkweather'
-'''
+"""
 
 Created by: Nathan Starkweather
 Created on: 02/01/2014
 Created in: PyCharm Community Edition
 
 
-'''
+"""
+__author__ = 'Nathan Starkweather'
 
 from .verbose import pfc, VerboseDict
 from .printdir import printdir
 from .safe_write import safe_pickle, safe_json
 from .gen_tools import make_module_header
+from .unique import unique_name
+
