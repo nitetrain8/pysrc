9b591ba896c3c033e4e3d01eaf4de8abd78b71ff
nitetrain8 <nathan.starkweather@gmail.com>
2014-03-12 11:45:53 -0700

    update tpidmany and unittest to include final test from 39 to 37 for eval_tpid analysis.

--- coursera_interactive/badmfc.py
+++ coursera_interactive/badmfc.py
@@ -7,29 +7,40 @@ import re
 from datetime import date, timedelta
 import sys
 
-mydate = date.today()
-td = timedelta(days=1)
-dateformat = "%B %d, %Y"
 
-mfp_doc = "C:\\Users\\Administrator\\Documents\\Programming\\mfp_test.txt"
-mfp_doc = "C:\\Users\\PBS Biotech\Documents\\Personal\\test files\\mfp.txt"
+def main():
+    mydate = date.today()
+    td = timedelta(days=1)
+    dateformat = "%B %d, %Y"
 
-with open(mfp_doc, 'r') as myfile:
-    text = myfile.read()
-    
+    mfp_doc = "C:\\Users\\Administrator\\Documents\\Programming\\mfp_test.txt"
+    mfp_doc = "C:\\Users\\PBS Biotech\Documents\\Personal\\test files\\mfp.txt"
+
+    with open(mfp_doc, 'r') as myfile:
+        text = myfile.read()
 
 
-    
-# for i in range(500):
-# mydate -= td
-# ds = mydate.strftime(dateformat)
-    
-find_date_ptrn = re.compile(r'(?<=id="date">)(.*)</h2>')
-find_cal_ptrn = re.compile(r'(?<=TOTAL:</td>)\s+<td>(\d*)(?:,*)(\d*)</td>')
 
-dates = re.findall(find_date_ptrn, text)
-cals = [int(''.join(x)) for x in re.findall(find_cal_ptrn, text)]
 
-for c, d in zip(cals, dates):
-    print(c)
-print(sum(cals)/len(cals))
+    # for i in range(500):
+    # mydate -= td
+    # ds = mydate.strftime(dateformat)
+
+    find_date_ptrn = re.compile(r'(?<=id="date">)(.*)</h2>')
+    find_cal_ptrn = re.compile(r'(?<=TOTAL:</td>)\s+<td>(\d*)(?:,*)(\d*)</td>')
+
+    dates = re.findall(find_date_ptrn, text)
+    cals = [int(''.join(x)) for x in re.findall(find_cal_ptrn, text)]
+
+    if len(dates) != len(cals):
+        raise ValueError("oh no!")
+
+    ave_daily = sum(cals) / len(cals)
+    return ave_daily, dates, cals
+
+if __name__ == '__main__':
+    main()
+
+
+
+
